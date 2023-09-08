b75d2fa93ff5da5a237504f44df4655d48c90415
Wesley-SR <wesley.sr@outlook.com>
2023-09-08 09:22:29 -0300

    Parâmetros gerais

---
+++ datas.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 20230902
+
+@author: Wesley
+"""
+
+import pandas as pd
+
+
+B = pd.DataFrame({'b_p1': [0]*I.shape[0], 
+                      'b_p2': [0]*I.shape[0], 
+                      'b_p3': [0]*I.shape[0],
+                      'b_p4': [0]*I.shape[0],
+                      'b_p5': [0]*I.shape[0],
+                      'b_total': [0]*I.shape[0]})
