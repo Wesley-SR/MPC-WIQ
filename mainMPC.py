3d6c916e1f4a3291b41648fe3fcecf9e5d8a4545
Wesley-SR <wesley.sr@outlook.com>
2023-09-11 18:35:58 -0300

    Inserção dos métodos de OptimizationQP e previsao

--- mainMPC.py
+++ mainMPC.py
@@ -5,7 +5,7 @@ Created on Sat Aug 22 21:39:00 2023
 """
 
 import time
-from QP_optimization import QPOptimization
+from optimization_QP import OptimizationQP
 
 class mainClass( ):
     def __init__(self, constants):
@@ -15,11 +15,10 @@ class mainClass( ):
         self.sampling_time_secondary = constants.loc[0, 'sampling_time_secondary']
         self.sampling_time_measurement = constants.loc[0, 'sampling_time_measurement']
         self.sampling_time_terciary = constants.loc[0, 'sampling_time_terciary']
+        
+        self.qp_otimization = OptimizationQP(constants)
 
-    def get_measurements(self):
-        # Simula a captura de dados
-        print("Capturando medidas...")
-        return [1, 2, 3, 4, 5]
+
 
     def run(self):
         start_time = time.time()
@@ -29,10 +28,10 @@ class mainClass( ):
         last_time_terciary = current_time
 
         while not self.stop_mpc:
-            
+
             time.sleep(self.time_sleep)
             current_time = time.time()
-            
+
             # Check if staying in island mode or connected mode
 
             # Take measurements
@@ -40,10 +39,14 @@ class mainClass( ):
                 print("Call self.get_measurements()")
                 self.get_measurements()
             current_time = time.time()
+            
+            # Predict PV
+            self.predict_pv()
+
 
             # Run tertiary optimization
             if current_time - last_time_terciary >= self.sampling_time_terciary:
-                self.tertiary_optimization()
+                self.qp_otimization.connected_tertiary_optimization(soc_bat_current, p_pv, p_load)
             current_time = time.time()
 
             # Run secundary optimization
@@ -52,8 +55,16 @@ class mainClass( ):
 
             # Check if it's time to stop the code
             if self.stop():
-                self.stop_mpc = True
+                self.stop_mpc = True 
 
+    def get_measurements(self):
+        # Simula a captura de dados
+        print("Capturando medidas...")
+        return [1, 2, 3, 4, 5]
+    
+    def predict_pv(self):
+        pass
+    
     def stop(self):
         self.stop_mpc = True
         
